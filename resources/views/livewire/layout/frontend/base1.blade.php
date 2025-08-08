afed314a66c5ac5e696c81b168a899b56b9ac452
@Rokon*77@ <rezaulkrn85@gmail.com>
2025-08-09 00:23:30 +0600

    add gooogle tag

--- resources/views/livewire/layout/frontend/base1.blade.php
+++ resources/views/livewire/layout/frontend/base1.blade.php
@@ -3,6 +3,15 @@
 
 
   <head>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TS7BZ52P');</script>
+    <!-- End Google Tag Manager -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -16,6 +25,11 @@
     <meta name="google-site-verification" content="UbkvPt_oAAGgpEZayP4bF_JG9vrdCLqhwF39WZ0gTDA" />
   </head>
   <body>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TS7BZ52P"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     <!-- Preloader-->
     @yield('preloader')
